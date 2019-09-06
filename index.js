dba0b066121dda22221ff3785d2ba139afa40eee
Felipe Lima <felipe@ferreiradelima.com>
2019-09-06 13:11:23 -0300

    pdf

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ const q = queue(async ({ res, html }, callback) => {
 
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
-    await page.pdf({ path: 'example.pdf', format: 'A4', printBackground: true }); //? Debug (gen pdf on project folder);
+    // await page.pdf({ path: 'example.pdf', format: 'A4', printBackground: true }); //? Debug (gen pdf on project folder);
     result.result = (await page.pdf({ format: 'A4', printBackground: true })).toString('base64');
   } catch (error) {
     console.log(error);
     result.status = false;
     result.error = JSON.stringify(error);
   } finally {
-    await browser.close();
+    browser.close().catch(console.error);
   }
 
   console.log('aaa')
